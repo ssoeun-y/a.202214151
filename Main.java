c6554dc7bf3b7a55018f6a597c2023f3f02ab58e
ssoeun-y <ssouen.yy@gmail.com>
2025-03-24 22:50:55 +0900

    fourth

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
-package studentA;
 public class Main {
 
-    public static void main(String[] args) {
-      System.out.println("천소은");
-    }
-  
+  public static void main(String[] args) {
+    System.out.println("홍길동");
+    World.run();
+    Hello.run();
   }
-  
+
+}
