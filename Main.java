260e3c5e7c36d7118178c3bbb63d117d75507770
ssoeun-y <ssouen.yy@gmail.com>
2025-03-24 22:57:39 +0900

    fifth

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("홍길동");
+    System.out.println("천소은");
     World.run();
-    Hello.run();
+    World.run2();
   }
 
 }
